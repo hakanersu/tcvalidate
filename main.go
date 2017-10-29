32fbb650cfeae05d5b085803e4659334e1941e7b
Hakan ERSU <hakanersu@gmail.com>
2017-10-29 20:56:03 +0300

    First

---
+++ main.go
@@ -0,0 +1,45 @@
+package validatetc
+
+import (
+	"strconv"
+)
+
+func validate(tcnumber string) bool {
+
+	runes := []rune(tcnumber)
+
+	odd, even, sum, rebuild := 0, 0, 0, ""
+
+	for i := 0; i < len(runes)-2; i++ {
+
+		a, _ := strconv.Atoi(string(runes[i]))
+
+		if string(runes[0]) == "0" {
+			return false
+		}
+
+		if a%2 == 0 {
+			odd += a
+		} else {
+			even += a
+		}
+
+		rebuild += string(runes[i])
+
+		sum += a
+	}
+
+	ten := (even*7 - odd) % 10
+
+	indexTen, _ := strconv.Atoi(string(runes[9]))
+
+	eleven := (sum + indexTen) % 10
+
+	build := string(rebuild) + strconv.Itoa(ten) + strconv.Itoa(eleven)
+
+	if build == tcnumber {
+		return true
+	}
+
+	return false
+}
